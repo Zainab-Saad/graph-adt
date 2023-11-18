1e261da6fe5f63293db40d25049a98b81bbbe310
attiyawaqar <awaqar.bese21seecs@seecs.edu.pk>
2023-11-18 22:53:52 +0500

    implemented concreteEdgesGraph

--- src/graph/ConcreteEdgesGraph.java
+++ src/graph/ConcreteEdgesGraph.java
@@ -3,18 +3,15 @@
  */
 package graph;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * An implementation of Graph.
  * 
  * <p>PS2 instructions: you MUST use the provided rep.
  */
-public class ConcreteEdgesGraph implements Graph<String> {
+public class ConcreteEdgesGraph implements Graph<String> 
+{
     
     private final Set<String> vertices = new HashSet<>();
     private final List<Edge> edges = new ArrayList<>();
@@ -30,32 +27,68 @@ public class ConcreteEdgesGraph implements Graph<String> {
     
     // TODO checkRep
     
-    @Override public boolean add(String vertex) {
-        throw new RuntimeException("not implemented");
+    @Override public boolean add(String vertex) 
+    {
+        if (vertices.contains(vertex))
+            return false;
+        
+        vertices.add(vertex);
+        return true;
     }
     
-    @Override public int set(String source, String target, int weight) {
-        throw new RuntimeException("not implemented");
+    @Override public int set(String source, String target, int weight) 
+    {
+        for (Edge edge:edges)
+        {
+            // if edge exists
+            if (edge.source.equals(source) && edge.destination.equals(target))
+            {
+                // updating the weight of the already existing edge
+                edge.weight = weight;
+                return edge.weight;
+            }
+        }
+        // if edge does not exist, create one
+        Edge edge = new Edge(source, target, weight);
+        edges.add(edge);
+        return edge.weight;
     }
     
-    @Override public boolean remove(String vertex) {
-        throw new RuntimeException("not implemented");
+    @Override public boolean remove(String vertex) 
+    {
+        return vertices.remove(vertex);
     }
     
-    @Override public Set<String> vertices() {
-        throw new RuntimeException("not implemented");
+    @Override public Set<String> vertices() 
+    {
+        return vertices;
     }
     
-    @Override public Map<String, Integer> sources(String target) {
-        throw new RuntimeException("not implemented");
+    @Override public Map<String, Integer> sources(String target) 
+    {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (Edge edge:edges)
+        {
+            // if target of that edge matches the specified target
+            if (edge.destination.equals(target))
+                map.put(edge.source, edge.weight);
+        }
+        return map;
     }
     
-    @Override public Map<String, Integer> targets(String source) {
-        throw new RuntimeException("not implemented");
+    @Override public Map<String, Integer> targets(String source) 
+    {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (Edge edge:edges)
+        {
+            // if target of that edge matches the specified target
+            if (edge.source.equals(source))
+                map.put(edge.destination, edge.weight);
+        }
+        return map;
     }
     
-    // TODO toString()
-    
+    // TODO toString() 
 }
 
 /**
@@ -66,18 +99,29 @@ public class ConcreteEdgesGraph implements Graph<String> {
  * <p>PS2 instructions: the specification and implementation of this class is
  * up to you.
  */
-class Edge {
-    
-    // TODO fields
-    
+class Edge 
+{
     // Abstraction function:
     //   TODO
     // Representation invariant:
     //   TODO
     // Safety from rep exposure:
     //   TODO
+    String source, destination;
+    int weight;
     
-    // TODO constructor
+    /**
+     * Creates an edge
+     * @param source label of source vertex
+     * @param destination label of destination vertex
+     * @param weight weight of the edge
+    */
+    Edge(String source, String destination, int weight)
+    {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
     
     // TODO checkRep
     
